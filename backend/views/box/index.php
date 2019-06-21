fcfdca39706c35580b2ec133db0f15826c4625de
Melissa Ho <32935782+melissaho97@users.noreply.github.com>
2019-06-21 21:34:24 +0800

    Box update

--- backend/views/box/index.php
+++ backend/views/box/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel backend\models\Box */
+/* @var $searchModel backend\models\BoxSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Boxes';
