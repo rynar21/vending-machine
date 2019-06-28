2749409599311879c1c4f0106dbc2c9c4afb1ee9
forgetof <51985057+forgetof@users.noreply.github.com>
2019-06-28 18:24:18 +0800

    store

---
+++ backend/views/store/indexfrom.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Store */
+
+$this->title = 'indexfrom';
+$this->params['breadcrumbs'][] = ['label' => 'Stores', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="store-indexfrom">
+
+
+
+
+
+
+
+
+
+</div>
+
+<style>
+.oneddd{
+  width: 600px;
+  height: 900px;
+  /* border: solid 1px red; */
+  text-align: center;
+  margin:0  auto;
+}
+
+</style>
