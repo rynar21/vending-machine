48779da367849abd56c2e79ce25d6b6da28c88b4
pcl99842 <51985084+pcl99842@users.noreply.github.com>
2019-07-03 19:31:12 +0800

    index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -60,37 +60,18 @@ $this->title = 'My Yii Application';
         </div>
       </div>
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+          <div class="col-sm-12">
+          <h3 class="font-weight-bold lead text-center" style="font-size: x-large; color:rgba(99,99,99,0.7);">
+            In this age of digital disruption, Konbini builds the turnkey solutions<br />
+            that help your business adapt and thrive.
+          </h3>
+          </div>
         </div>
-
+        <div class="row">
+			<div class="col-sm-12 text-center">
+			     <h1 class="ont-weight-bold section-heading text-uppercase" style="color:rgba(65,80,255)">Solutions</h1>
+						<!-- <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
+			</div>
+		</div>
     </div>
 </div>
