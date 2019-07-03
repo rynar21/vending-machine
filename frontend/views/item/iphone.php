927b7db21733f045b5ba857f40e3f6b8efd50ccd
forgetof <870248053@qq.com>
2019-07-03 12:01:45 +0800

    Create iphone.php

---
+++ frontend/views/item/iphone.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\ActiveForm;
+use yii\db\Query;
+use yii\data\BaseDataProvider;
+
+
+/* @var $this yii\web\View */
+$this->title = 'My Yii Application';
+?>
+
+<div class="item-index">
+
+
+</div>
