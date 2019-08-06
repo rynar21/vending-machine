ef07c84137c3ba244683c81b7a03ed972719f437
Melissa Ho <32935782+melissaho97@users.noreply.github.com>
2019-08-07 00:34:38 +0800

    Revert "update migration scripts"
    This reverts commit 59f7b7bd73b9b12c360b50bcccf5e705b6173060.

--- console/migrations/m190621_100127_sales_record.php
+++ console/migrations/m190621_100127_sales_record.php
@@ -16,11 +16,11 @@ class m190621_100127_sales_record extends Migration
 
         $this->createTable('sale_record', [
           'id' =>$this->primaryKey(),
+          'trans_id' =>$this->integer(),
           'store_id' =>$this->integer()->notNull(),
           'box_id' =>$this->integer()->notNull(),
           'item_id' =>$this->integer()->notNull(),
-          'sell_price' =>$this->float(10,2)->notNull(),
-          'status' =>$this->smallInteger()->notNull(),
+          'status' =>$this->smallInteger()->notNull()->defaultValue(9),
           'created_at' => $this->integer()->notNull(),
           'updated_at' => $this->integer()->notNull(),
       ]);
