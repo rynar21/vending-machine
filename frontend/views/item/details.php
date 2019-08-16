526bdcc88c0b08362bb6a11acccf1d8c7a13f7ef
Melissa Ho <32935782+melissaho97@users.noreply.github.com>
2019-08-16 12:11:05 +0800

    update imageUrl to imagePath

--- frontend/views/item/details.php
+++ frontend/views/item/details.php
@@ -10,7 +10,7 @@ use yii\helpers\Url;
     <div class="col-sm-offset-4 col-sm-4 col-lg-offset-2 col-lg-8 item_details">
         <div>
             <div class="pull-left item_details_image">
-                <img src="<?= Yii::getAlias('@imageUrl').'/'.$item->image ?>" class="img-responsive center-block"/>
+                <img src="<?= Yii::getAlias('@imagePath').'/'.$item->image ?>" class="img-responsive center-block"/>
             </div>
             <div class="pull-left text-left item_details_name" >
                 <br/>
