eed4c4de532586e36624de1c1d4da86e894d1af9
Melissa Ho <32935782+melissaho97@users.noreply.github.com>
2019-08-29 12:34:59 +0800

    itemcontroller备注

--- backend/views/item/_form_create.php
+++ backend/views/item/_form_create.php
@@ -15,6 +15,7 @@ use yii\web\JsExpression;
 
 <div class="item-form">
 
+        <!-- 运行 Yii ActiveForm 框架 -->
         <?php $form = ActiveForm::begin(); ?>
 
         <!-- 盒子 ID -->
@@ -37,22 +38,25 @@ use yii\web\JsExpression;
         <div class="row">
             <div class="col-sm-12">
               <?php
-              $test = Product::find()
-                ->select(['name as value', 'name as  label','id as id'])
-                ->asArray()
-                ->all();?>
-                <?= AutoComplete::widget([
-                      'name' => 'name',
-                      'clientOptions' => [
-                          'source' => $test,
-                          'autoFill' => true,
-                          // 'select' => new JsExpression("function( event, ui )) {
-                          //     $('#product').val(ui.product.id);
-                          //  }"),
-                      ],
-                  ]);
-              //$form->field($model, 'product_id')->dropDownList(ArrayHelper::map(Product::find()->all(), 'id', 'name')) ?>
+                  $test = Product::find()
+                    ->select(['name as value', 'name as  label','id as id'])
+                    ->asArray()
+                    ->all();?>
+                    <?= AutoComplete::widget([
+                          'name' => 'name',
+                          'clientOptions' => [
+                              'source' => $test,
+                              'autoFill' => true,
+                              // 'select' => new JsExpression("function( event, ui )) {
+                              //     $('#product').val(ui.product.id);
+                              //  }"),
+                          ],
+                      ]); ?>
+
               <?= Html::activeHiddenInput($model, 'id')?>
+
+
+              <?php //echo $form->field($model, 'product_id')->dropDownList(ArrayHelper::map(Product::find()->all(), 'id', 'name')); ?>
             </div>
         </div>
 
