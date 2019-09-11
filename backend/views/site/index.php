68ff305770d261508cf8fc802abdc12ee8577128
rynar21 <51905303+rynar21@users.noreply.github.com>
2019-09-11 13:20:40 +0800

    chart-js-index

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,51 +1,90 @@
 <?php
+use common\models\SaleRecord;
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'chart';
+
+$labels = [];
+$data = [];
+
+
+$model_time = SaleRecord::find()->where(['status' => 10])->all();
+
+
+for ($i=0; $i <= 7 ; $i++) {
+  $labels[] = date('"Y-m-d "', strtotime(-24*60*60*$i-$i .'seconds'));
+  sort($labels);
+}
+
+for ($i=0; $i < count($labels); $i++) {
+  $model_count = SaleRecord::find()
+  ->where(['between', 'updated_at', strtotime(-24*60*60*$i-$i .'seconds'),strtotime(-24*60*60*(1-$i)-$i .'seconds')   ])
+  ->andWhere(['status'=> 10])
+  ->count();
+$data[]=$model_count;
+}
+
+// for ($i=0; $i <= 7 ; $i++) {
+//   $labels[] = date('"Y-m-d "', strtotime(-$i .'days'));
+//   sort($labels);
+// }
+//
+// for ($i=0; $i < count($labels); $i++) {
+//   $model_count = SaleRecord::find()
+//   ->where(['between', 'updated_at', strtotime(-$i.'days'),strtotime(1-$i .'days')   ])
+//   ->andWhere(['status'=> 10])
+//   ->count();
+// $data[]=$model_count;
+// }
+  print_r($labels);
+
+  print_r($data)
 ?>
+
+<canvas id="myChart"></canvas>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+    <script>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+    <?php //foreach ($model_time as $updateTime) {
+      // echo $updateTime->updated_at . ",";
+      //echo "'" . date('Y-m-d H:i:s', $updateTime->updated_at)."',";
+  //  } ?>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+    // The data for our dataset
+        data: {
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        labels: [<?= implode($labels, ',') ?>],
+        datasets: [{
+            label: 'No. of success transaction',
+            // backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [<?= implode($data, ',') ?> ]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+
+
+
+    }
+});
+</script>
             </div>
         </div>
 
