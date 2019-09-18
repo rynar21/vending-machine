ee2dd85bea7f57c1b2626562709790cff71e4deb
pcl99842 <51985084+pcl99842@users.noreply.github.com>
2019-09-18 15:00:46 +0800

    gai

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,51 +1,86 @@
 <?php
 /* @var $this yii\web\View */
-
+use common\models\SaleRecord;
+use common\models\Item;
+use common\models\Product;
 $this->title = 'chart';
+$product=[];
+$data_type=[];
+$array = [];
+
+
+        $product = Product::find()->all();
+        for ($i=1; $i<=count($product) ; $i++)
+        {
+            $data_item = Item::find()
+            ->Where([
+                'between',
+                'updated_at',
+                strtotime(date('Y-m-d',strtotime(-29 .' day'))),
+                strtotime(date('Y-m-d',strtotime(0 .' day')))
+            ])
+            ->where(['product_id'=>$i,'status'=>10])
+            ->all();
+            foreach ($data_item as $item)
+            {
+                $data_product = Product::find()->where(['id' => $item->product_id])->one();
+            }
+            $data_type[] = "'".$data_product->sku."'";
+            $count_data[] = count($data_item);
+        }
+        // $count_data[] = count($data_type);
+        for ($z=0; $z <=count($count_data)-1; $z++)
+        {
+            // if (!empty($count_data[$z]&&$data_type[$z]))
+            // {
+                $array[]=array($count_data[$z],$data_type[$z]);
+            // }
+        }
+        for ($y=0; $y <count($count_data)-1 ; $y++) {
+            array_multisort(array_column($array,'0'),SORT_DESC,$array);
+        }
+        $count=array_slice($array,0,5);
 ?>
 
 <canvas id="myChart"></canvas>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 
 <div class="site-index">
-
     <div class="body-content">
-
         <div class="row">
-            <div class="col-lg-4">
-    <script>
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
-
-    // The data for our dataset
-        data: {
-
-        labels: [<?= implode($labels, ',') ?>],
-        datasets: [
-            {
-                label: 'No. of success transaction',
-                backgroundColor: 'transparent',
-                borderColor: 'rgb(100, 50, 150)',
-                data: [<?= implode($data, ',') ?> ]
-            },
-            {
-                label: 'Total Amount (RM)',
-                backgroundColor: 'transparent',
-                borderColor: 'rgb(0, 0,0 )',
-                data: [<?= implode($data_amount, ',') ?> ]
-            }
-    ]
-    },
-
-    // Configuration options go here
-    options: {
-    }
-});
-</script>
-            </div>
+                <?php
+                    // print("<pre>");
+                    // // print_r(count($product));
+                    // print_r($count_data);
+                    // print_r($data_type);
+                    // print_r($array);
+                    // print("</pre>");
+                ?>
         </div>
-
     </div>
 </div>
+<script>
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+            labels: [<?= implode($labels, ',') ?>],
+            datasets: [
+                            {
+                                label: 'No. of success transaction',
+                                backgroundColor: 'transparent',
+                                borderColor: 'rgb(100, 50, 150)',
+                                data: [<?= implode($data, ',') ?> ]
+                            },
+                            {
+                                label: 'Total Amount (RM)',
+                                backgroundColor: 'transparent',
+                                borderColor: 'rgb(0, 0,0 )',
+                                data: [<?= implode($data_amount, ',') ?> ]
+                            }
+                        ]
+        },
+            options: {
+            }
+        });
+</script>
