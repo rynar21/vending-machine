8682ddb384369768775783866552d78a4d15146f
Lo Wei Chin <weichin.lo@gmail.com>
2020-03-27 20:43:32 +0800

    fix asset code

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -4,6 +4,7 @@ return [
         '@upload'       => '/app/backend/web/mel-img/',
         '@imagePath'    => 'http://localhost:21110/products',
         '@url'          => 'C:\Users\user\Desktop\up',
+        '@static'       => 'http://localhost:21110/',
     ],
     'components' => [
         'mailer' => [
