4860910798ee8e3a536202723a6ab562dc436c17
rynar21 <51905303+rynar21@users.noreply.github.com>
2020-03-30 11:22:09 +0800

    hide-salerecord-fake-data

--- console/migrations/fake/m190621_100127_sales_record.php
+++ console/migrations/fake/m190621_100127_sales_record.php
@@ -29,27 +29,27 @@ class m190621_100127_sales_record extends Migration
           'item_name'=>$this->string(50)->notNull(),
           'box_code'=>$this->string(20)->notNull(),
       ]);
-      $this->sale_data();
+      // $this->sale_data();
     }
 
-    public function sale_data()
-    {
-        $model = new SaleRecord();
-        $model->id = 1;
-        $model->order_number = uniqid();
-        $model->store_id = 1;
-        $model->box_id = 1;
-        $model->item_id = 1;
-        $model->sell_price = 2.00;
-        $model->unique_id = uniqid();
-        $model->created_at = "1584595153";
-        $model->updated_at = "1584595153";
-        $model->store_name = "Rynar Ashchaser shd. bhd";
-        $model->item_name = "N95 Mask";
-        $model->box_code = 1;
-        $model->status = 10;
-        $model->save();
-    }
+    // public function sale_data()
+    // {
+    //     $model = new SaleRecord();
+    //     $model->id = 1;
+    //     $model->order_number = uniqid();
+    //     $model->store_id = 1;
+    //     $model->box_id = 1;
+    //     $model->item_id = 1;
+    //     $model->sell_price = 2.00;
+    //     $model->unique_id = uniqid();
+    //     $model->created_at = "1584595153";
+    //     $model->updated_at = "1584595153";
+    //     $model->store_name = "Rynar Ashchaser shd. bhd";
+    //     $model->item_name = "N95 Mask";
+    //     $model->box_code = 1;
+    //     $model->status = 10;
+    //     $model->save();
+    // }
 
     /**
      * {@inheritdoc}
