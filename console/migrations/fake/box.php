be4a035361169af19f160800ac88298edda788a0
rynar21 <51905303+rynar21@users.noreply.github.com>
2020-03-31 19:28:36 +0800

    temp remove fake data

--- console/migrations/fake/box.php
+++ console/migrations/fake/box.php
@@ -6,26 +6,17 @@ use common\models\Box;
 /**
  * Class m190620_063254_box
  */
-class m190620_063254_box extends Migration
+class box extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-          $this->createTable('box', [
-              'id' => $this->primaryKey(),
-              'code' => $this->string()->notNull(),
-              'status' => $this->smallInteger()->notNull()->defaultValue(2),
-              'created_at' => $this->integer()->notNull(),
-              'updated_at' => $this->integer()->notNull(),
-              'store_id' => $this->integer(),
-              'hardware_id' => $this->string()
-          ]);
-
-          for ($i=1; $i < 7 ; $i++) {
-              $this->box_data($i);
-          }
+        for ($i=1; $i < 7 ; $i++)
+        {
+            $this->box_data($i);
+        }
     }
 
     public function box_data($i)
