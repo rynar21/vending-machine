25c89e833fc90efa328c9d5c1b4f2e53a7306ccb
rynar21 <51905303+rynar21@users.noreply.github.com>
2020-04-02 12:13:19 +0800

    update PaymentController

--- frontend/controllers/PaymentController.php
+++ frontend/controllers/PaymentController.php
@@ -17,27 +17,28 @@ class PaymentController extends Controller
 {
     public $imodel;
     public $enableCsrfValidation = false;
+
     public function actionCreateOrder()
     {
 
         $salerecord_id = $_POST['salerecord_id'];
         $price = $_POST['price'];
         $data = [
-             'merchantId' => Yii::$app->spay->merchantId,
-             'curType' => 'RM',
-             'notifyURL' => 'https://google.com/',
-             'merOrderNo' => $salerecord_id,
-             'goodsName' => '',
-             'detailURL' => "http://localhost:20080/payment/check?id=$salerecord_id",
-             'orderAmt' => $price,
-             'remark' => '',
-             'transactionType' => '1',
+            'merchantId' => Yii::$app->spay->merchantId,
+            'curType' => 'RM',
+            'notifyURL' => 'https://google.com/',
+            'merOrderNo' => $salerecord_id,
+            'goodsName' => '',
+            'detailURL' => "http://localhost:20080/payment/check?id=$salerecord_id",
+            'orderAmt' => $price,
+            'remark' => '',
+            'transactionType' => '1',
         ];
-        $data               = json_encode($data, 320);
+        $data             = json_encode($data, 320);
         //$response_data = Yii::$app->spay->createOrder($data);
-        $response_data = SarawakPay::post('https://spfintech.sains.com.my/xservice/H5PaymentAction.preOrder.do', $data);
-        if ($response_data) {
+        $response_data    = SarawakPay::post('https://spfintech.sains.com.my/xservice/H5PaymentAction.preOrder.do', $data);
 
+        if ($response_data) {
             $get_response = json_decode($response_data);
             $referenceNo  = $get_response->{'merOrderNo'};
             $token        = $get_response->{'securityData'};
@@ -49,48 +50,43 @@ class PaymentController extends Controller
 
     }
 
-
-
     // 判断 交易订单 的状态
     public function actionCheck($id)
     {
-
         $model = SaleRecord::find()->where(['order_number' => $id])->one();
         $item_model = item::find()->where(['id' => $model->item_id])->one();
         $data = [
-             'merchantId' => 'M100001040',
-             'merOrderNo' => $id,
+            'merchantId' => 'M100001040',
+            'merOrderNo' => $id,
         ];
-        $data      = json_encode($data, 320);
-        $string    = SarawakPay::post('https://spfintech.sains.com.my/xservice/H5PaymentAction.queryOrder.do', $data);
-        $array     = json_decode($string);
+        $data          = json_encode($data, 320);
+        $string        = SarawakPay::post('https://spfintech.sains.com.my/xservice/H5PaymentAction.queryOrder.do', $data);
+        $array         = json_decode($string);
         $orderStatus   = $array->{'orderStatus'};
         $orderAmt      = $array->{'orderAmt'};
+
         if ($model!=null)
         {
-            if ($orderStatus == 0) {
+            if ($orderStatus == 0)
+            {
                 return $this->render('/sale-record/create', [
                     'item_model' => $item_model,
                     'model' => $model,
                     'id' => $id,
                 ]);
             }
-            elseif ($orderStatus == 1) {
-
+            elseif ($orderStatus == 1)
+            {
                 $this->add_queue([
                     'store_id'=>$model->store_id,
                     'action' =>$model->box->hardware_id,
                 ]);
                 //return $this->runAction('sale-record/paysuccess',['id'=>$id]); //error
-
-                return $this->redirect(['sale-record/paysuccess',
-                       'id'=>$id,
-                   ]);
+                return $this->redirect(['sale-record/paysuccess','id'=>$id]);
             }
-            elseif($orderStatus == 2 || $orderStatus == 4) {
-                return $this->redirect(['sale-record/payfailed',
-                       'id' => $id,
-                ]);
+            elseif($orderStatus == 2 || $orderStatus == 4)
+            {
+                return $this->redirect(['sale-record/payfailed','id' => $id,]);
             }
             else
             {
