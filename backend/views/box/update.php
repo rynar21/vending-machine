3fb9189e8a9e6d964ff264e4689b1b00990cebfe
rynar21 <51905303+rynar21@users.noreply.github.com>
2020-04-16 10:37:14 +0800

    remove box-update breadcrumbs

--- backend/views/box/update.php
+++ backend/views/box/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\Box */
 
-$this->title = 'Update Box: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Boxes', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Update Box: ' . $model->code;
+// $this->params['breadcrumbs'][] = ['label' => 'Boxes', 'url' => ['index']];
+// $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+// $this->params['breadcrumbs'][] = 'Update';
 ?>
 
 <div class="box-update">
