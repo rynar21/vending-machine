e73f6db78c49c1625cf911add411de6c467957b2
forgetof <870248053@qq.com>
2020-06-03 15:22:14 +0800

    api  update

--- api/controllers/CronController.php
+++ api/controllers/CronController.php
@@ -15,46 +15,9 @@ class CronController extends Controller
 
     public function actionRequest()
     {
-        $records = SaleRecord::find()->where([
-                'status' => SaleRecord::STATUS_PENDING,
-            ])->all();
+        $this->queryPendingOrder();
 
-        if ($records)
-        {
-            foreach ($records as $record)
-            {
-                $data = [
-                    'merchantId' => Yii::$app->spay->merchantId,
-                    'merOrderNo' => $record->order_number,
-                ];
-
-                $response_data = Yii::$app->spay->checkOrder($data);
-                $array         = json_decode($response_data);
-                $orderStatus   = $array->{'orderStatus'};
-
-                if ($orderStatus == 1)
-                {
-                    $record->success();
-                }
-
-                if ($orderStatus == 4)
-                {
-                    $record->failed();
-                }
-
-            }
-        }
-
-         Yii::$app->slack->Posturl([
-             'url'=>'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=216b4e9e-8404-457c-8996-fd171fa3224f',
-             'data'=>[
-                     "msgtype" => "text",
-
-                     "text" => [
-                         "content" => date('Y-m-d H:i:s', time()),
-                     ],
-             ],
-         ]);
+        $this->testAPI();
 
         return [
             'data' => [
@@ -62,6 +25,71 @@ class CronController extends Controller
             ]
         ];
     }
+
+    private function queryPendingOrder()
+    {
+        $records = SaleRecord::find()->where([
+                'status' => SaleRecord::STATUS_PENDING,
+        ])->all();
+
+        if ($records)
+        {
+            foreach ($records as $record)
+            {
+                $this->querySpOrderAPI($record->order_number);
+            }
+        }
+
+    }
+
+    private function querySpOrderAPI($order_number)
+    {
+        $data = [
+            'merchantId' => Yii::$app->spay->merchantId,
+            'merOrderNo' => $order_number,
+        ];
+
+        $response_data = Yii::$app->spay->checkOrder($data);
+        $array         = json_decode($response_data);
+        $orderStatus   = $array->{'orderStatus'};
+
+        $this->checkOrderStatus($order_number,$orderStatus);
+    }
+
+    private function checkOrderStatus($order_number,$orderStatus)
+    {
+        $record = SaleRecord::findOne(['order_number' => $order_number]);
+
+        if ($orderStatus == 1)
+        {
+            $record->success();
+        }
+
+        if ($orderStatus == 4)
+        {
+            $record->failed();
+        }
+    }
+
+    private function testAPI()
+    {
+        $ip = Yii::$app->request->userIP; //主机IP
+        Yii::$app->slack->Posturl([
+            'url' => 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=216b4e9e-8404-457c-8996-fd171fa3224f',
+            'data' => [
+                    "msgtype" => "text",
+
+                    "text" => [
+                        "content" => date('Y-m-d H:i:s', time()). ' '."IP:".$ip,
+                    ],
+            ],
+        ]);
+    }
+
+
+
+
+
 
 
 }
