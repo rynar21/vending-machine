17f32ea23936ba8af21a91fa0d8fefb52dcce2ce
rynar21 <51905303+rynar21@users.noreply.github.com>
2020-06-05 15:47:34 +0800

    update testAPI function

--- common/models/SaleRecord.php
+++ common/models/SaleRecord.php
@@ -244,7 +244,7 @@ class SaleRecord extends \yii\db\ActiveRecord
 
     public  function queryPendingOrder()
     {
-        $sum2 = 0;
+        $count_number = 0;
         $data = [];
         $records = SaleRecord::find()->where([
                 'status' => SaleRecord::STATUS_PENDING,
@@ -255,36 +255,34 @@ class SaleRecord extends \yii\db\ActiveRecord
             foreach ($records as $record)
             {
                 if ($record->executeUpdateStatus()) {
-                    $sum2 += 0;
+                    $count_number += 0;
                 }
 
-                $sum2 += 1;
+                $count_number += 1;
 
                 if ($record->status == SaleRecord::STATUS_PENDING)
                 {
-                    $sum2 = $sum2 - 1;
+                    $count_number = $count_number - 1;
                 }
 
                 if ($record->status == SaleRecord::STATUS_SUCCESS)
                 {
-                    $data[] = array($record->order_number, '    Failed' );
-
+                    $data[] = $record->order_number . ' Success';
                 }
 
                 if ($record->status == SaleRecord::STATUS_FAILED)
                 {
-                    $data[] = array($record->order_number, '    Failed' );
-
+                    $data[] = $record->order_number . ' Failed';
                 }
+
             }
         }
-        //return false;
-        return $this->testAPI(count($records),$sum2,$data);
+
+        return $this->testAPI(count($records), $count_number, $data);
     }
 
-    private  function testAPI($sum1,$sum2,$data)
+    private  function testAPI($count_array, $count_number, $data)
     {
-        //$ip = gethostbyname(gethostname()); //主机IP
 
         Yii::$app->slack->Posturl([
             'url' => 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=11057651-b2bf-42a9-9eb3-760049e1ac87',
@@ -292,9 +290,9 @@ class SaleRecord extends \yii\db\ActiveRecord
                     "msgtype" => "text",
 
                     "text" => [
-                        "content" => "查询支付中订单:".$sum1."条"."\n".
-                        "处理:".$sum2."条"."\n".
-                        "OrderNumber:".'    '."Status:"."\n".($data),
+                        "content" => "查询支付中订单:".$count_array."条"."\n".
+                        "处理:".$count_number."条"."\n".
+                        "OrderNumber:".'    '."Status:"."\n". implode("\n", $data),
                     ],
             ],
         ]);
