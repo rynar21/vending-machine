543178df6c66c8a2a0697ed5038f02795782fae3
Rynar <51905303+rynar21@users.noreply.github.com>
2021-01-07 14:36:01 +0800

    house keeping box view

--- backend/views/box/update.php
+++ backend/views/box/update.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Box */
@@ -15,8 +16,36 @@ $this->title = 'Update Box: ' . $model->code;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form_update', [
-        'model' => $model,
-    ]) ?>
+    <div class="box-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+            <?= $form->field($model, 'store_id')->textInput(['disabled' => true]) ?>
+
+            <div class="row">
+                 <div class="col-sm-12">
+                      <b> Box Code </b>
+                  </div>
+                  <div class="col-sm-12">
+                      <?= $form->field($model, 'code', [
+                        'template' => '<div class="input-group"><span class="input-group-addon">'. $model->prefix .'</span>{input}</div>',
+                        ])->textInput(['disabled' => true]) ?>
+                 </div>
+                 <div class = "col-sm-12">
+                     <?= $form->field($model, 'hardware_id')->textInput(['disabled' => false]) ?>
+                 </div>
+            </div>
+            <div class="row form-group">
+                  <div class="col-sm-1 col-xs-3">
+                        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+                  </div>
+                  <div class="col-sm-1 col-xs-3">
+                        <?= Html::a('Cancel', ['store/view', 'id'=> $model->store_id], ['class' => 'btn btn-danger']) ?>
+                 </div>
+            </div>
+
+        <?php ActiveForm::end(); ?>
+
+</div>
 
 </div>
