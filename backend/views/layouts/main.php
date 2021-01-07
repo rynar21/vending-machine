66d2f22213c00bb45ce36f5b16b230ba16b31efc
forgetof <870248053@qq.com>
2021-01-07 14:01:14 +0800

    把 导航栏 移去左边

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -22,46 +22,99 @@ AppAsset::register($this);
         <?php $this->registerCsrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
+
+        <style>
+        [v-cloak] {display:none;}
+        .navbar {
+            color: #FFF;
+            border: none;
+        }
+        .nav > li > a{
+            border:none;
+            color: #FFF;
+            text-decoration: none;
+        }
+        .nav > li > a:hover, .nav > li > a:active, .nav .open > a:focus {
+            border:none;
+            color:#fff;
+            background-color: #666;
+        }
+        .nav .open > a, .nav .open > a:hover, .nav .open > a:focus {
+            background-color:  #666;
+            border-color: #337ab7;
+        }
+        .dropdown-menu > li > a:hover, .nav > .active > a {
+            background-color:  #333333;
+            color: #fff;
+        }
+        .dropdown-menu > .active > a{
+            background-color:  #fff;
+            color: black;
+        }
+        .dropdown-menu > .active > a:hover{
+            background-color:  #333333;
+            color:#fff;
+            text-decoration: none;
+        }
+
+        .grid {
+            display: grid;
+            grid-gap: 8px;
+            grid-template-columns: 1fr;
+            grid-template-rows: min-content;
+        }
+
+        .grid-2 {
+            grid-template-columns: repeat(6, 1fr);
+        }
+
+        .grid-3 {
+            grid-template-columns: repeat(4, 1fr);
+        }
+
+        .grid-4 {
+            grid-template-columns: repeat(3, 1fr);
+        }
+
+        .grid-6 {
+            grid-template-columns: repeat(2, 1fr);
+        }
+
+        @media (min-width: 768px) {
+            .grid-sm-4 {
+                grid-template-columns: repeat(3, 1fr);
+            }
+        }
+
+    </style>
     </head>
 
     <body>
       <?php $this->beginBody() ?>
 
-        <div class="wrap ">
+      <div class="grid grid-2" style="grid-template-rows:100vh;padding-right:10px;">
+        <div class="" style="padding:0px;background-color:#222;overflow-y: auto;">
+            <div class="navbar navbar-inverse" role="navigation">
+
             <?php
-            NavBar::begin([
-                'brandLabel' => 'Vending Machine',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            if (!Yii::$app->user->isGuest)
-            {
-            $menuItems = [
-                // ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'User Management', 'url' => ['/user/index'], 'visible' => Yii::$app->user->can('allowAssign')],
-                ['label' => 'Finance', 'url' => ['/finance/index'], 'visible' => Yii::$app->user->can('allowReport')],
-                ['label' => 'Vending Machine', 'url' => ['/store/index'], 'visible' => Yii::$app->user->can('staff')],
-                ['label' => 'Record', 'url' => ['/sale-record/index'], 'visible' => Yii::$app->user->can('allowRecord')],
-                ['label' => 'Product', 'url' => ['/product/index'], 'visible' => Yii::$app->user->can('allowProduct')],
-                ['label' => 'Change Password', 'url' => ['/user/change-password']],
-                ['label' => 'Logout', 'url' => ['/site/logout'], 'linkOptions' => ['data-method' => 'post']]
-            ];
-            }
-            if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login'],
-            ];
-
-            }
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => $menuItems,
-            ]);
-            NavBar::end();
+                $items = [
+                    ['label' => 'User Management', 'url' => ['/user/index'], 'visible' => Yii::$app->user->can('allowAssign')],
+                    ['label' => 'Finance', 'url' => ['/finance/index'], 'visible' => Yii::$app->user->can('allowReport')],
+                    ['label' => 'Vending Machine', 'url' => ['/store/index'], 'visible' => Yii::$app->user->can('staff')],
+                    ['label' => 'Record', 'url' => ['/sale-record/index'], 'visible' => Yii::$app->user->can('allowRecord')],
+                    ['label' => 'Product', 'url' => ['/product/index'], 'visible' => Yii::$app->user->can('allowProduct')],
+                    ['label' => 'Change Password', 'url' => ['/user/change-password']],
+                    ['label' => 'Logout', 'url' => ['/site/logout'], 'linkOptions' => ['data-method' => 'post']]
+                ];
+            
+                echo Nav::widget([
+                    'options' => ['class' => 'nav'],
+                    'items' => $items
+                ]);
             ?>
-
-            <div class="container">
+            </div>
+        </div>
+            <div style="grid-column:2/7;overflow-y: auto;padding-top:8px">
                 <?= Breadcrumbs::widget([
                     'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
                 ]) ?>
@@ -70,11 +123,7 @@ AppAsset::register($this);
             </div>
         </div>
 
-        <footer class="footer">
-            <div class="container">
-
-            </div>
-        </footer>
+       
       <?php $this->endBody() ?>
     </body>
   </html>
