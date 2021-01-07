fd5f1d3aa7849972612a2ec2b2ef5328dee8eff4
forgetof <870248053@qq.com>
2021-01-07 14:45:46 +0800

    删除没有用到的代码

--- backend/models/SaleRecordSearch.php
+++ backend/models/SaleRecordSearch.php
@@ -56,37 +56,16 @@ class SaleRecordSearch extends SaleRecord
 
         $dataProvider = new ActiveDataProvider([
             'query' =>  $query->orderBy(['id'=>SORT_DESC]),
-            //'query' => SaleRecord::find()->where(['status'=>10])->all(),
         ]);
 
         $this->load($params);
 
         if (!$this->validate())
         {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
             return $dataProvider;
         }
 
-        // $domain = strstr($this->status, 's');
-        // if (strstr($this->status, 's') || strstr($this->status, 'S'))
-        // {
-        //     $this->stu = SaleRecord::STATUS_SUCCESS;
-        // }
-        //
-        // if (strstr($this->status, 'f') || strstr($this->status, 'F'))
-        // {
-        //     $this->stu = SaleRecord::STATUS_FAILED;
-        // }
-        //
-        // if (strstr($this->status, 'p') || strstr($this->status, 'P'))
-        // {
-        //     $this->stu = SaleRecord::STATUS_PENDING;
-        // }
-
-        //$query->joinWith('item');
         $query->andFilterWhere([
-            //'id' => $this->id,
             'box_id' => $this->box_id,
             'item_id' => $this->item_id,
             'status' => $this->status,
@@ -121,59 +100,5 @@ class SaleRecordSearch extends SaleRecord
         return $dataProvider;
     }
 
-
-    //单个商店某一天的所有订单
-    public function searchStoreAllsalerecord($params,$array)//$store_id,$date
-    {
-        $store_id   = ArrayHelper::getValue($array,'store_id',Null);
-        $date       = ArrayHelper::getValue($array,'date',Null);
-        $box_id     = ArrayHelper::getValue($array,'box_id',Null);
-
-        $query      = SaleRecord::find();
-
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
-
-        $this->load($params);
-        if (!$this->validate())
-        {
-            return $dataProvider;
-        }
-
-        if (strstr($this->status, 's') || strstr($this->status, 'S'))
-        {
-            $this->stu = SaleRecord::STATUS_SUCCESS;
-        }
-
-        if (strstr($this->status, 'f') || strstr($this->status, 'F'))
-        {
-            $this->stu = SaleRecord::STATUS_FAILED;
-        }
-
-        if (strstr($this->status, 'p') || strstr($this->status, 'P'))
-        {
-            $this->stu = SaleRecord::STATUS_PENDING;
-        }
-
-        $query->andFilterWhere([
-            'status' => SaleRecord::STATUS_SUCCESS,
-            'store_id' => $store_id,
-            'box_id' => $box_id,
-            'box_code' => $this->box_code,
-            'order_number' =>$this->order_number,
-            //'store_name' =>$this->store_name,
-            //'item_name' =>$this->item_name,
-        ]);
-
-        //$query->joinWith('product');
-         $query->andFilterWhere(['between', 'created_at', $date, $date + 86399])
-         ->andFilterWhere(['like','item_name', $this->itemname])
-         ->orFilterWhere(['like', 'store_name', $this->store_name])
-         ->orFilterWhere(['like','box_code', $this->text])
-         ->orFilterWhere(['like','unique_id', $this->unique_id]);
-
-        return $dataProvider;
-    }
 
 }
