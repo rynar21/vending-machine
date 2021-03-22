a7fb54430a5675e17b8ee2b3571277da13e4f6c7
Rynar <51905303+rynar21@users.noreply.github.com>
2021-03-22 13:51:30 +0800

    Fixed #7 (#8)

--- api/controllers/TestController.php
+++ api/controllers/TestController.php
@@ -33,4 +33,22 @@ class TestController extends Controller
         
         return "Incorrect Password";
     }
+
+    // requested by He lin for temp solution
+    public function actionOpenAll($key=null)
+    {
+        $id = 1;
+        $hardware_id = "00OK";
+        $set_password = "4242564";
+
+        if ($key){
+            if ($set_password == $key){
+                Queue::push($id, $hardware_id);
+    
+                return "Success Push: ". $hardware_id;
+            }
+        }
+
+        return "Incorrect Password";
+    }
 }
